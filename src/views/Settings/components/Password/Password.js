cca990f9a8d43737df6ffe1134833b323d7b2abd
mmirek98 <michal.mirek98@gmail.com>
2020-04-05 18:56:18 +0200

    Send PATCH request when both fields match and logout user from current session

--- src/views/Settings/components/Password/Password.js
+++ src/views/Settings/components/Password/Password.js
@@ -12,8 +12,12 @@ import {
   TextField
 } from '@material-ui/core';
 import validate from 'validate.js';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import isEmpty from 'helpers/isEmpty';
-
+import { patch } from 'actions/patch';
+import { setCurrentUser } from 'redux/authReducer';
+import LogoutDialog from 'components/Dialogs/LogoutDialog';
 
 const useStyles = makeStyles(() => ({
   root: {}
@@ -33,7 +37,14 @@ const schema = {
 }
 
 const Password = props => {
-  const { className, ...rest } = props;
+  const { 
+    className,
+    userObject,
+    patchAction,
+    history,
+    setCurrentUserAction,
+    ...rest
+  } = props;
 
   const classes = useStyles();
 
@@ -44,11 +55,14 @@ const Password = props => {
     errors: {}
   });
 
+  const [openDialog, setOpenDialog] = useState(false);
+  const [updateData, setUpdateData] = useState(false);
+
   useEffect(() => {
     const errors = validate(formState.values, schema) || {};
 
     if (formState.values.password !== formState.values.confirm) {
-      errors.confirm = ['Confirm password must match'];
+      errors.confirm = ['Both fields must match'];
     }
 
     setFormState(formState => ({
@@ -76,63 +90,127 @@ const Password = props => {
 
   const hasError = field => formState.touched[field] && !!formState.errors[field];
 
+  const handleUpdatePassword = event => {
+    event.preventDefault();
+    setOpenDialog(!openDialog);
+    updatePassword();
+  }
+
+  const updatePassword = () => {
+    if (updateData) {
+      patch().catch(e => console.error(e.message));
+    }
+  }
+
+  useEffect(() => {
+    updatePassword();
+  }, [updateData]);
+
+  const logout = async () => {
+    setCurrentUserAction({});
+    localStorage.removeItem('jwtToken');
+    history.push('/sign-in');
+  };
+
+  const patch = async () => {
+    const body = { ...formState.values };
+    delete body.confirm;
+    const token = localStorage.getItem('jwtToken');
+    const email = userObject.email;
+
+    if (token) {
+      const response = await patchAction(email, body, token);
+
+      if (response.status === 200) {
+        logout();
+      } else {
+        throw new Error('Error during patching. There is no token available');
+      }
+    }
+  };
+
   return (
-    <Card
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-      <form>
-        <CardHeader
-          subheader="Update password"
-          title="Password"
+    <>
+      {openDialog && (
+        <LogoutDialog
+          openDialog={openDialog}
+          setOpenDialog={setOpenDialog}
+          setUpdateData={setUpdateData}
         />
-        <Divider />
-        <CardContent>
-          <TextField
-            error={hasError('password')}
-            helperText={
-              hasError('password') ? formState.errors.password[0] : null
-            }
-            fullWidth
-            label="Password"
-            name="password"
-            onChange={handleChange}
-            type="password"
-            value={formState.password}
-            variant="outlined"
+      )}
+      <Card
+        {...rest}
+        className={clsx(classes.root, className)}
+      >
+        <form>
+          <CardHeader
+            subheader="Update password"
+            title="Password"
           />
-          <TextField
-            error={hasError('confirm')}
-            helperText={
-              hasError('confirm') ? formState.errors.confirm[0] : null
-            }
-            fullWidth
-            label="Confirm password"
-            name="confirm"
-            onChange={handleChange}
-            style={{ marginTop: '1rem' }}
-            type="password"
-            value={formState.confirm}
-            variant="outlined"
-          />
-        </CardContent>
-        <Divider />
-        <CardActions>
-          <Button
-            disabled={!formState.isValid}
-            color="primary"
-            variant="outlined"
-          >
-            Update
-          </Button>
-        </CardActions>
-      </form>
-    </Card>
+          <Divider />
+          <CardContent>
+            <TextField
+              error={hasError('password')}
+              helperText={
+                hasError('password') ? formState.errors.password[0] : null
+              }
+              fullWidth
+              label="Password"
+              name="password"
+              onChange={handleChange}
+              type="password"
+              value={formState.password}
+              variant="outlined"
+            />
+            <TextField
+              error={hasError('confirm')}
+              helperText={
+                hasError('confirm') ? formState.errors.confirm[0] : null
+              }
+              fullWidth
+              label="Confirm password"
+              name="confirm"
+              onChange={handleChange}
+              style={{ marginTop: '1rem' }}
+              type="password"
+              value={formState.confirm}
+              variant="outlined"
+            />
+          </CardContent>
+          <Divider />
+          <CardActions>
+            <Button
+              disabled={!formState.isValid}
+              onClick={handleUpdatePassword}
+              color="primary"
+              variant="outlined"
+            >
+              Update
+            </Button>
+          </CardActions>
+        </form>
+      </Card>
+    </>
   );
 };
 
 Password.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  patchAction: PropTypes.func,
+  userObject: PropTypes.object,
+  history: PropTypes.object,
+  setCurrentUserAction: PropTypes.func,
 };
 
-export default Password;
+const mapStateToProps = state => {
+  const { user } = state;
+
+  return {
+    userObject: user
+  };
+};
+
+export default connect(mapStateToProps, {
+  patchAction: patch,
+  setCurrentUserAction: setCurrentUser
+})(withRouter(Password));
