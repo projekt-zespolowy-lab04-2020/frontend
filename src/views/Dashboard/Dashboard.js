d67a8ba62a8b532c4aa78fe9f1492999a0e75359
mmirek98 <michal.mirek98@gmail.com>
2020-04-17 16:25:50 +0200

    Delete UsersByDevice and LatestProduct components

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -8,8 +8,6 @@ import {
   TasksProgress,
   TotalProfit,
   LatestSales,
-  UsersByDevice,
-  LatestProducts,
   LatestOrders
 } from './components';
 
@@ -66,36 +64,18 @@ const Dashboard = () => {
         </Grid>
         <Grid
           item
-          lg={8}
+          lg={12}
           md={12}
-          xl={9}
+          xl={12}
           xs={12}
         >
           <LatestSales />
         </Grid>
         <Grid
           item
-          lg={4}
-          md={6}
-          xl={3}
-          xs={12}
-        >
-          <UsersByDevice />
-        </Grid>
-        <Grid
-          item
-          lg={4}
-          md={6}
-          xl={3}
-          xs={12}
-        >
-          <LatestProducts />
-        </Grid>
-        <Grid
-          item
-          lg={8}
+          lg={12}
           md={12}
-          xl={9}
+          xl={12}
           xs={12}
         >
           <LatestOrders />
