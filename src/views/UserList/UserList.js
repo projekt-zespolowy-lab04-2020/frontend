08efcdc488a35e5c1d72b78049a202ce769ef59e
Morphikss <44949530+Morphikss@users.noreply.github.com>
2020-05-13 12:10:17 +0200

    searcher_logic (#16)
    * searcher_logic
    
    * Changes after review
    
    Co-authored-by: sebekxp <p15sebek@gmail.com>

--- src/views/UserList/UserList.js
+++ src/views/UserList/UserList.js
@@ -17,12 +17,22 @@ const UserList = () => {
   const classes = useStyles();
 
   const [users] = useState(mockData);
+  const [searchResults, setSearchResults] = useState(mockData);
+
+  const onSearchChange = event => {
+    const inputValue = event.target.value.toLowerCase();
+    const results = users.filter(user => {
+      const fullName = user.firstName + ' ' + user.lastName;
+      return fullName.toLowerCase().includes(inputValue);
+    });
+    setSearchResults(results);
+  };
 
   return (
     <div className={classes.root}>
-      <UsersToolbar />
+      <UsersToolbar onSearchChange={onSearchChange} />
       <div className={classes.content}>
-        <UsersTable users={users} />
+        <UsersTable users={searchResults} />
       </div>
     </div>
   );
