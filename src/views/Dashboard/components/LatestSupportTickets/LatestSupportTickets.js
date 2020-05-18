4aeb0b23fc9ddf7ec0909c7828f39ba156fa1094
mmirek98 <michal.mirek98@gmail.com>
2020-05-18 19:06:06 +0200

    Add sorting by date and by status

--- src/views/Dashboard/components/LatestSupportTickets/LatestSupportTickets.js
+++ src/views/Dashboard/components/LatestSupportTickets/LatestSupportTickets.js
@@ -24,6 +24,7 @@ import { ADMIN } from 'helpers/types';
 import { getTickets } from 'actions/tickets/getTickets';
 import { getTicketByID } from 'actions/tickets/getTicketByID';
 import Spinner from 'components/Spinner/Spinner';
+import moment from 'moment';
 
 
 const useStyles = makeStyles(theme => ({
@@ -46,11 +47,6 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const statusColors = {
-  delivered: 'success',
-  pending: 'info',
-  refunded: 'danger'
-};
 
 const LatestSupportTickets = props => {
   const { 
@@ -65,6 +61,8 @@ const LatestSupportTickets = props => {
 
   const [hasTickets, setHasTicketsFlag] = useState(false);
   const [searchResults, setSearchResults] = useState([]);
+  const [sortByDateDesc, setSortByDateDesc] = useState(true);
+  const [sortByOpenStatus, setSortByOpenStatus] = useState(true);
 
   const getTicketAsync = async (ID, token) => {
     const response = await getTicketByIDAction(ID, token);
@@ -92,11 +90,14 @@ const LatestSupportTickets = props => {
       const res = await response.json();
       if (response.status === 200) {
         getAllUserTickets(res, token)
+          .then(tickets => tickets.sort((t1, t2) => (
+            moment(t2.ticket.createDate, 'DD-MM-YYYY') - moment(t1.ticket.createDate, 'DD-MM-YYYY')
+          )))
           .then(tickets => {
             if (!tickets.length) setHasTicketsFlag(true);
             // store tickets list in local variable instead in redux store 
             // -> there is error while navigating to /support after fetching tickets in this component
-            setSearchResults([...tickets])
+            setSearchResults([...tickets]);
           })
           .catch(e => console.error(e.message));
       } else {
@@ -116,6 +117,34 @@ const LatestSupportTickets = props => {
     }
   }, [userObject]);
 
+  const handleSortTicketsByDate = () => {
+    const dateFormat = 'DD-MM-YYYY';
+    if (sortByDateDesc) {
+      setSearchResults([
+        ...searchResults.sort((t1, t2) => (
+          moment(t1.ticket.createDate, dateFormat) - moment(t2.ticket.createDate, dateFormat)
+        ))
+      ]);
+    } else {
+      setSearchResults([
+        ...searchResults.sort((t1, t2) => (
+          moment(t2.ticket.createDate, dateFormat) - moment(t1.ticket.createDate, dateFormat)
+        ))
+      ]);      
+    }
+
+    setSortByDateDesc(!sortByDateDesc);
+  };
+
+  const handleSortTicketsByStatus = () => {
+    if (sortByOpenStatus) {
+      setSearchResults([...searchResults.sort((t1, _) => t1.ticket.closed ? -1 : 1)]);
+    } else {
+      setSearchResults([...searchResults.sort((t1, _) => t1.ticket.closed ? 1 : -1)]);
+    }
+    setSortByOpenStatus(!sortByOpenStatus);
+  };
+
   return (
     <Card {...rest} className={clsx(classes.root, className)}>
       <CardHeader title="Latest Support Tickets" />
@@ -123,26 +152,38 @@ const LatestSupportTickets = props => {
       <CardContent className={classes.content}>
         <PerfectScrollbar>
           <div className={classes.inner}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell>Author</TableCell>
-                  <TableCell sortDirection="desc">
-                    <Tooltip enterDelay={300} title="Sort">
-                      <TableSortLabel active direction="desc">
-                        Date
-                      </TableSortLabel>
-                    </Tooltip>
-                  </TableCell>
-                  <TableCell>Closed</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {(!searchResults.length && !hasTickets)
-                  ? <Spinner /> 
-                  : searchResults.map((data, index) => {
-                    const { ticket, comments } = data;
+            {(!searchResults.length && !hasTickets) ?
+              <Spinner /> :
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell>Author</TableCell>
+                    <TableCell sortDirection="desc">
+                      <Tooltip enterDelay={300} title="Sort">
+                        <TableSortLabel
+                          active
+                          direction={sortByDateDesc ? 'desc' : 'asc'}
+                          onClick={handleSortTicketsByDate}>
+                            Date
+                        </TableSortLabel>
+                      </Tooltip>
+                    </TableCell>
+                    <TableCell sortDirection="desc">
+                      <Tooltip enterDelay={300} title="Sort">
+                        <TableSortLabel
+                          active
+                          direction={sortByOpenStatus ? 'desc' : 'asc'}
+                          onClick={handleSortTicketsByStatus}>
+                            Status
+                        </TableSortLabel>
+                      </Tooltip>
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {searchResults.map((data, index) => {
+                    const { ticket } = data;
                     return (
                       <TableRow hover key={index}>
                         <TableCell>{ticket.id}</TableCell>
@@ -153,13 +194,13 @@ const LatestSupportTickets = props => {
                           {ticket.createDate}
                         </TableCell>
                         <TableCell>
-                          {ticket.closed ? 'Yes' : 'No'}
+                          {ticket.closed ? 'Closed' : 'Open'}
                         </TableCell>
                       </TableRow>
                     );             
                   })}
-              </TableBody>
-            </Table>
+                </TableBody>
+            </Table>}
           </div>
         </PerfectScrollbar>
       </CardContent>
