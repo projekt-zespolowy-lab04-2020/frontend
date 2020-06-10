c8bf064e1bb74a11f5bab1589d4aeb9e430562dc
Mateusz <mateusz.obierak@gmail.com>
2020-06-10 02:27:22 +0200

    delete LatestSales component

--- src/views/Dashboard/components/index.js
+++ src/views/Dashboard/components/index.js
@@ -1,6 +1,5 @@
 export { default as Budget } from './Budget';
 export { default as LatestSupportTickets } from './LatestSupportTickets';
-export { default as LatestSales } from './LatestSales';
 export { default as TasksProgress } from './TasksProgress';
 export { default as TotalProfit } from './TotalProfit';
 export { default as TotalUsers } from './TotalUsers';
